87da5422aa6dbf564fb751471a5a7851577dcb0a
Hung Tran <hung.p.tran@opswat.com>
2023-01-04 23:12:49 +0700

    add /long

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New(fiber.Config{
@@ -8,6 +12,13 @@ func main() {
 	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Get("/long", func(c *fiber.Ctx) error {
+		for i := 0; i < 10000; i++ {
+			fmt.Println(i)
+		}
 		return c.SendString("Hello, World!")
 	})
 
